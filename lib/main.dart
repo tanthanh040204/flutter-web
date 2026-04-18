7deb9c6db32707eb8213d0f65ad09f3231d8091e
TranHaiz <tranhai002456@gmail.com>
2026-04-18 08:41:56 +0700

    update: Optimize code

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,10 @@ import 'package:provider/provider.dart';
 
 import 'config/app_theme.dart';
 import 'firebase_options.dart';
-import 'models/bluetooth_device_info.dart';
 import 'providers/auth_provider.dart';
-import 'providers/bluetooth_provider.dart';
 import 'providers/device_provider.dart';
 import 'providers/fleet_provider.dart';
-import 'providers/fota_provider.dart';
 import 'providers/maintenance_provider.dart';
-import 'providers/route_provider.dart';
-import 'providers/streaming_provider.dart';
 import 'providers/trip_provider.dart';
 import 'screens/app_bootstrap.dart';
 import 'services/mqtt_service.dart';
@@ -52,41 +47,15 @@ class RouteTrackerApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(create: (_) => DeviceProvider()),
         ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (_) => RouteProvider()),
-        ChangeNotifierProvider(create: (_) => BluetoothProvider()),
-        ChangeNotifierProvider(create: (_) => StreamingProvider()),
-        ChangeNotifierProvider(create: (_) => FotaProvider()),
         ChangeNotifierProvider(create: (_) => FleetProvider()),
         ChangeNotifierProvider(create: (_) => TripProvider()),
         ChangeNotifierProvider(create: (_) => MaintenanceProvider()),
       ],
-      child: Builder(
-        builder: (context) {
-          final bluetoothProvider = Provider.of<BluetoothProvider>(context);
-          final streamingProvider = Provider.of<StreamingProvider>(
-            context,
-            listen: false,
-          );
-          final fotaProvider = Provider.of<FotaProvider>(
-            context,
-            listen: false,
-          );
-
-          bluetoothProvider.addListener(() {
-            if (bluetoothProvider.connectionState ==
-                AppBluetoothConnectionState.disconnected) {
-              streamingProvider.clearAll();
-              fotaProvider.clearAll();
-            }
-          });
-
-          return MaterialApp(
-            title: 'Route Tracker',
-            debugShowCheckedModeBanner: false,
-            theme: AppTheme.lightTheme,
-            home: const AppBootstrap(),
-          );
-        },
+      child: MaterialApp(
+        title: 'Route Tracker',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        home: const AppBootstrap(),
       ),
     );
   }
